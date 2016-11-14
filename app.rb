ca94faa4d3be5b18e95c39dbf4b930515cd96b7e
jdalpert <josh1994bv@gmail.com>
2016-11-14 12:13:33 -0800

    figured out problem

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require "rubygems"
 require "sinatra"
+require "erb"
+
+enable :sessions
 
 get "/" do
-	"Hello world"
+	session[:name] = ""
+	erb :test
 end
+
+get "/:name" do
+	session[:name] += params[:name] + "<br>"
+	@name = session[:name]
+	erb :test
+end
+
+post "/form" do
+	@name = "#{params[:message]}"
+	redirect to("/#{@name}")
+end 
